4c11b62607960aa367f75efde2de9ccae873b46c
Oluwaseun <adunmofestus@gmail.com>
2023-09-29 02:53:29 +0100

    line break inserted

--- script/main.js
+++ script/main.js
@@ -2,12 +2,17 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes")
+}
+showNotes();
+
 function updateStorage() {
     localStorage.setItem("notes", notesContainer.innerHTML);
 }
 
 createBtn.addEventListener("click", ()=>{
-    let inputBox = document.createElement("p");
+    let inputBox = document.createElement("P");
     let img = document.createElement("img")
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
@@ -27,5 +32,11 @@ createBtn.addEventListener("click", ()=>{
                     updateStorage();
                 }
             })
+        }
+    })
+    document.addEventListener("keydown", event =>{
+        if(event.key === "Enter"){
+            document.execCommand("insertLineBreak");
+            event.preventDefault();
         }
     })
